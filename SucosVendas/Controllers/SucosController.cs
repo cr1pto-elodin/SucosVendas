55fed51ff101fa4755829af5e54a2c0bff5922af
Victor do Vale Polck <victor.polck@inspira.br>
2023-02-03 17:21:00 -0300

    Vamos testar

---
+++ SucosVendas/Controllers/SucosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+
+namespace SucosVendas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SucosController : ControllerBase
+    {
+        public static int Quantidade = 0;
+        public Dictionary<int, Sucos> sucos = new Dictionary<int, Sucos>();
+        [HttpGet]
+        public Dictionary<int,Sucos> Get()
+        {
+            return sucos;
+        }
+
+        [HttpPost]
+        public HttpStatusCode Post([FromBody] string values)
+        {
+            Sucos suco = JsonConvert.DeserializeObject<Sucos>(values);
+            if (String.IsNullOrEmpty(suco.Marca) || String.IsNullOrEmpty(suco.Sabor) || String.IsNullOrEmpty(suco.QuantidadeMl))
+                return HttpStatusCode.BadRequest;
+            Quantidade++;
+            sucos.Add(Quantidade, suco);
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ SucosVendas/Sucos.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SucosVendas
+{
+    public class Sucos
+    {
+        [JsonProperty("marca")]
+        public string Marca { get; private set; }
+        [JsonProperty("sabor")]
+        public string Sabor { get; private set; }
+        [JsonProperty("quantidadeMl")]
+        public string QuantidadeMl { get; private set; }
+
+
+    }
+}
