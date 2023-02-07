df65146b919a253d38404225e440f300fd64a0e1
Victor do Vale Polck <victor.polck@inspira.br>
2023-02-07 16:58:06 -0300

    Adição do EF e contextos de classe

---
+++ SucosVendas/Contexts/SucosContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SucosVendas.Contexts
+{
+    public class SucosContext : DbContext
+    {
+        public SucosContext()  : base("SucosContext")
+        {
+        }
+
+        public DbSet<Sucos> Sucos { get; set; }
+        public DbSet<Funcao> Funcoes { get; set; }
+    }
+}
---
+++ SucosVendas/Usuarios.cs
@@ -0,0 +1,11 @@
+﻿namespace SucosVendas
+{
+    public class Usuarios
+    {
+        public Int64 ID { get; set; }
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public Funcao Funcao { get; set; }
+    }
+}
